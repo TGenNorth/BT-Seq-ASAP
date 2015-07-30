0859d833574fad69c48f1322740aeaabcf50de78
DarwinW13 <dlemmer@tgen.org>
2015-07-30 10:17:04 -0700

    doc splitting issue

--- asap/outputCombiner.py
+++ asap/outputCombiner.py
@@ -62,7 +62,10 @@ def main(argv=None): # IGNORE:C0111
     program_version = "v%s" % __version__
     program_build_date = str(__updated__)
     program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
-    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
+    if __name__ == '__main__':
+        program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
+    else:
+        program_shortdesc = __doc__.split("\n")[1]    
     program_license = '''%s
 
   Created by TGen North on %s.
