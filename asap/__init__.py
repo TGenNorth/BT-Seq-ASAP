e1a3605a48bbf7d96201259a41dbf05040563de8
Jason Travis <jtravis@tgen.org>
2018-07-18 12:05:57 -0700

    refactor version to be defined by setuptools_scm write_to file

--- asap/__init__.py
+++ asap/__init__.py
@@ -3,11 +3,4 @@
 __author__ = 'Darrin Lemmer'
 __email__ = 'dlemmer@tgen.org'
 
-# http://setuptools.readthedocs.io/en/latest/setuptools.html#adding-support-for-revision-control-systems
-from pkg_resources import get_distribution, DistributionNotFound
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    __version__ = 'dev'
-    pass
+from .version import version as __version__
