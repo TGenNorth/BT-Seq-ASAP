02e976d406118ea43b7a98ada7527d2859fbeed6
Zane Koch <zkoch@tnorth-login.tgen.org>
2019-07-23 10:48:49 -0700

    progress messages for user in formater.sh

--- asap/formater.sh
+++ asap/formater.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #$1 name of .xml from asap
-
+#$2 path to first output transform
+#$3 path to second output transform
 
 export PYTHONHASHSEED=1
 
@@ -12,11 +13,18 @@ reformatedName=$(echo "$1" | sed 's/.xml/_Reformated.xml/g')
 reReformatedName=$(echo $reformatedName | sed 's/.xml/_2.xml/g')
 
 formatOutput -s $2  -x $1 -o ./$formatOutputName 
+
+echo "first output transform done"
+
 reformatXML -x $1
+
+echo "reformatting done"
 
 assay_specific_alleles -x $reformatedName -o $reReformatedName
 
+echo "re-reformatting done"
+
 formatOutput -s $3 -x $reReformatedName
 
-
+echo "done"
 
